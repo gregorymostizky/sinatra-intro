6c44a38a87c52bf88a9c2e8b4b94c6bcf148da77
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-07-10 18:33:04 +0300

    testing

---
+++ 07-monitor-rackup/test/test_example.rb
@@ -0,0 +1,31 @@
+require 'test/unit'
+require 'rack/test'
+
+Dir.glob('../lib/**.rb') do |libfile|
+  require File.dirname(__FILE__) + '/' + libfile  # just load all
+end
+
+require File.dirname(__FILE__)+'/../monitor'
+
+
+class MyTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Monitor
+  end
+
+  def test_default
+    get '/'
+    assert last_response.body =~ /Simple Monitoring Application Demo/
+  end
+
+  def test_post
+    post '/report/test/ok'
+    post '/report/test/ok'
+    get '/'
+    assert last_response.body =~ /test(.*)ok/
+
+  end
+
+end
